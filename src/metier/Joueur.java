3b913f5eb6e6386546e5ca33c06f924b0a540150
Henintsoa Razanamasy <Henintsoa.Razanamasy@etu.univ-lehavre.fr>
2024-06-11 10:52:43 +0200

    maj joueur

--- src/metier/Joueur.java
+++ src/metier/Joueur.java
@@ -1,5 +1,19 @@
-package metier;
+package  src.metier;
 
 public class Joueur {
 	
+	private String nom;
+	private int numJoueur;
+	private String type;
+
+	public Joueur(String nom, int numJoueur , String type)
+	{
+		this.nom = nom;
+		this.numJoueur = numJoueur;
+		this.type = type;
+	}
+
+	public String getNom() { return this.nom;}
+	public int getNumeroJoueur() { return this.numJoueur;}
+	public String getType() { return this.type;}
 }
