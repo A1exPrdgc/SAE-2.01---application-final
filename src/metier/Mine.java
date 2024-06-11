c01ca24d9145ce70588482f843f28a3d7efb9d3e
Henintsoa Razanamasy <Henintsoa.Razanamasy@etu.univ-lehavre.fr>
2024-06-11 09:43:31 +0200

    Ajout Mine

--- src/metier/Mine.java
+++ src/metier/Mine.java
@@ -2,5 +2,27 @@ package metier;
 
 public class Mine 
 {
-	
+	private int x ;
+	private int y ; 
+	private Ressource ressource;
+	private int numero ;
+	private char region;
+
+	public Mine(int x , int y , int numero, char region)
+	{
+		this.x =x;
+		this.y=y;
+		this.numero = numero;
+		this.region = region;
+	}
+
+	public boolean ajouterRessource(Ressource r)
+	{
+		if(this.ressource == null )
+			{
+				this.ressource = r ;
+				return true;
+			}
+		return false;
+	}
 }
