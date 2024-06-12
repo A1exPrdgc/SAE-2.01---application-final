a174a1b05cdbd8c58f624baa316f733c6d2dd4db
Henintsoa Razanamasy <Henintsoa.Razanamasy@etu.univ-lehavre.fr>
2024-06-12 16:35:12 +0200

    mod

--- src/Etape2/ihm/FrameFicheScore.java
+++ src/Etape2/ihm/FrameFicheScore.java
@@ -3,7 +3,6 @@ package Etape2.ihm;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JTable;
-import javax.swing.table.AbstractTableModel;
 
 import Etape2.Controleur; 
 
@@ -11,6 +10,7 @@ public class FrameFicheScore extends JFrame
 {
 	private Controleur ctrl ;
 	private JTable tableau ;
+	private Object[][] data;
 
 	public FrameFicheScore()//Controleur ctrl)
 	{
@@ -21,23 +21,35 @@ public class FrameFicheScore extends JFrame
 
 		this.setSize(500,500);
 
-		tableau.setValueAt("Fiche de Score",0, 1);
-		tableau.setValueAt("Corporation", 1, 1);
-		tableau.setValueAt("Syndicat Astral", 1, 2);
-		tableau.setValueAt("Points Route", 3, 0);
-		tableau.setValueAt("Points des Mines", 5, 0);
-		tableau.setValueAt( new ImageIcon("images/etoile.png"),6,0);
-		tableau.setValueAt("S/Total", 14, 0);
-		tableau.setValueAt("Plateau Individuel", 16, 0);
-		tableau.setValueAt("Score Pieces", 17, 0);
-		tableau.setValueAt("Score des Colonnes", 18, 0);
-		tableau.setValueAt("Scores des Lignes", 19, 0);
-		tableau.setValueAt("S/Total", 20, 0);
-		tableau.setValueAt("Jetons Possesion restants", 22, 0);
-		tableau.setValueAt("Bonus(10)", 23, 0);
-		tableau.setValueAt("Total", 25, 0);
+		data = new Object[30][3];
+
+		data[0][1] = "Fiche de Score";
+		data[1][1] = "Corporation";
+		data[1][2] = "Syndicat Astral";
+		data[3][0] = "Points Route";
+		data[5][0] = "Points des Mines";
+		data[6][0] =  new ImageIcon("images/etoile.png");
+		data[7][0] =  new ImageIcon("images/etoile_bleu.png");
+		data[8][0] =  new ImageIcon("images/losange.png");
+		data[9][0] =  new ImageIcon("images/vert.png");
+		data[10][0] =  new ImageIcon("images/tuile.png");
+		data[11][0] =  new ImageIcon("images/marron.png");
+		data[12][0] = "S/Total";
+		data[14][0] = "Plateau Individuel";
+		data[16][0] = "Score Pieces";
+		data[17][0] = "Scores des Colonnes";
+		data[18][0] = "Scores des Lignes";
+		data[19][0] = "S/Total";
+		data[21][0] = "Jetons Possession restants";
+		data[22][0] = "Bonus";
+		data[24][0] = "Total";
+
+		tableau = new JTable(data, new String[] {"", "", ""});
 
 		this.add(tableau);
+
+
+		
 
 		this.setVisible(true);
 	}
