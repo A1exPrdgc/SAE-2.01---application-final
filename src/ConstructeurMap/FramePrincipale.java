d9852d28a97c5c4e04337868d1ff3f2eca09c259
Antoine Lechasles <antoine.lechasles@etu.univ-lehavre.fr>
2024-06-13 16:38:37 +0200

    mise a jour ConstructeurMap

--- src/ConstructeurMap/FramePrincipale.java
+++ src/ConstructeurMap/FramePrincipale.java
@@ -2,29 +2,27 @@ package ConstructeurMap;
 
 import javax.swing.JFrame;
 
-
-
 public class FramePrincipale extends JFrame
 {
-    private PanelPrincipale panelPrincipale;
-    private Controleur ctrl;
-    
-    public FramePrincipale(Controleur ctrl)
-    {
-        this.ctrl = ctrl;
-        this.setSize(1000, 1000); 
-		this.setTitle("Plateau jeu"); 
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-		this.setVisible(true); 
 
-		this.add(new PanelPrincipale(this.ctrl));
-		
+	private Controleur ctrl;
+	private PanelPrincipale panel;
+
+	public FramePrincipale(Controleur ctrl)
+	{
+		this.ctrl = ctrl;
+		this.setSize(1000, 1000);
+		this.setTitle("Plateau jeu");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+		this.panel = new PanelPrincipale(ctrl);
+		this.add(this.panel);
+
 	}
-   
+
 	public static void main(String[] args)
 	{
 		new FramePrincipale(new Controleur());
 	}
 
-	
 }
--- src/ConstructeurMap/PanelPrincipale.java
+++ src/ConstructeurMap/PanelPrincipale.java
@@ -1,8 +1,5 @@
 package ConstructeurMap;
-
 import javax.swing.JPanel;
-
-
 
 import java.awt.BasicStroke;
 import java.awt.Color;
@@ -13,49 +10,45 @@ import java.io.BufferedReader;
 import java.util.List;
 import java.util.ArrayList;
 
-
 public class PanelPrincipale extends JPanel
 {
-    private Image imgFond;
-    private Controleur ctrl;
-    private Graphics2D g2;
-    private int posX , posY;
-    private JPanel panel;
+	private Image imgFond;
+	private Controleur ctrl;
+	private Graphics2D g2;
+	private int posX, posY;
+	private JPanel panel;
 
-    /*private List<Routes> lstSectionRoutes; */
+	/* private List<Routes> lstSectionRoutes; */
 
-    public PanelPrincipale(Controleur ctrl)
-    {
-       this.ctrl = ctrl;
-     
-       this.imgFond = getToolkit().getImage("../images/PlateauPrincipale.png");
+	public PanelPrincipale(Controleur ctrl)
+	{
+		this.ctrl = ctrl;
 
-       /*this.lstSectionRoutes = new ArrayList<Routes>();*/
-       
-       
-    }
+		this.imgFond = getToolkit().getImage("../images/PlateauPrincipale.png");
 
-    public void majDessin()
-    {
-        this.repaint();
-    }
+		/* this.lstSectionRoutes = new ArrayList<Routes>(); */
 
-    public void dessinerAretes(Graphics g) 
-    {
-       
-    }
+	}
 
-    public void paintComponent(Graphics g)
-    {
-        super.paintComponent(g);
-        g2 = (Graphics2D) g;
+	public void majDessin()
+	{
+		this.repaint();
+	}
 
-        if (imgFond != null) {
-            g2.drawImage(imgFond, 0, 0, this.getWidth(), this.getHeight(), this);
-        }
+	public void dessinerAretes(Graphics g)
+	{
 
+	}
 
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		g2 = (Graphics2D) g;
 
-        
-    }
+		if (imgFond != null)
+		{
+			g2.drawImage(imgFond, 0, 0, this.getWidth(), this.getHeight(), this);
+		}
+
+	}
 }
