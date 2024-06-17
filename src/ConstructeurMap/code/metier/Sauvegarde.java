8b8686ebce95c6cf590f775f8d4ded4ffec17abc
Antoine Lechasles <antoine.lechasles@etu.univ-lehavre.fr>
2024-06-17 16:04:50 +0200

    mise a jour sauvegarde

--- src/ConstructeurMap/code/metier/Sauvegarde.java
+++ src/ConstructeurMap/code/metier/Sauvegarde.java
@@ -18,6 +18,11 @@ public class Sauvegarde
 			for (Mine mine : mines)
 			{
 				writer.println(mine.getX() + "," + mine.getY() + "," +mine.getNumMine() + "," + mine.getRegion().name().charAt(0) + "," + mine.getVisit());
+
+				if(mine.getRessource().getType() instanceof Minerai)
+				{
+					writer.println(((Minerai) mine.getRessource().getType()).name());
+				}
 			}
 		} catch (IOException e)
 		{
@@ -39,17 +44,4 @@ public class Sauvegarde
 			e.printStackTrace();
 		}
 	}
-/*
- * public void chargerDonnees() { try (BufferedReader reader = new
- * BufferedReader(new FileReader("ConstructeurMap/mines.txt"))) { String line;
- * boolean isReadingRoutes = false; while ((line = reader.readLine()) != null) {
- * if (line.equals("Mines:")) continue; String[] parts = line.split(","); if
- * (!isReadingRoutes) { // Créer une nouvelle mine et l'ajouter à la liste Mine
- * mine = new Mine(Integer.parseInt(parts[0]), parts[1],
- * Integer.parseInt(parts[2]), Integer.parseInt(parts[3])); lstMines.add(mine);
- * } else { // Créer une nouvelle route et l'ajouter à la liste Route route =
- * new Route(mines.get(Integer.parseInt(parts[0])),
- * mines.get(Integer.parseInt(parts[1])), Integer.parseInt(parts[2]));
- * lstRoutes.add(route); } } } catch (IOException e) { e.printStackTrace(); } }
- */
 }
